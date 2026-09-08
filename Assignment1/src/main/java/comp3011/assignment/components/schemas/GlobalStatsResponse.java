d2182d50bc22acb5be453e8ba66f8daf9e122098
Dang Khoa Nguyen <dangkhoa04052005@gmail.com>
2026-09-08 23:26:15 +0930

    Create ShutdownResponse, UptimeResponse and GlobalStatsResponse.

---
+++ Assignment1/src/main/java/comp3011/assignment/components/schemas/GlobalStatsResponse.java
@@ -0,0 +1,4 @@
+package comp3011.assignment.components.schemas;
+
+public record GlobalStatsResponse(int inputTokens, int outputTokens) 
+{}
---
+++ Assignment1/src/main/java/comp3011/assignment/components/schemas/UptimeResponse.java
@@ -0,0 +1,4 @@
+package comp3011.assignment.components.schemas;
+
+public record UptimeResponse(String utcServerStart, String utcNow, double serverUptimeResponse) 
+{}
